2aab44e31b40a181316f918a204c932dbd8e4448
ying1488 <yingecteach@gmail.com>
2024-04-17 20:41:36 +1000

    express

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
-
 const app = express();
 
-app.use((req, res) => {
-  console.log("we got a new request")
+// app.use((req, res) => {
+//   console.log("we got a new request")
+//   res.send('<h1>this is my page</h1>')
+// })
+
+app.get('/cats', (req, res) => {
+  console.log("cat request!!")
+  res.send('<h1>cat page </h1>')
+})
+
+
+app.post('/cats', (req, res) => {
+  res.send('post request to /cats!! this is different')
+})
+app.get('/dogs', (req, res) => {
+  console.log("dog request")
+  res.send('<h1>dog page </h1>')
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>welcome to homepage</h1>')
+})
+
+
+// /cats =>'meow'
+// /dogs =>'woof
+// '/'  
+
+app.get('*', (req, res) => {
+  res.send('i dont know that path')
 })
 
 app.listen(3000, () => {
-  console.log("listening on port 3000!")
+  res.send(`listening on port 3000!`)
 })
